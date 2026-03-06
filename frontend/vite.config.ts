eb4d1b8af2305ec9d0ff7ae290a2810d28c583a7
christophersiem <christopher.siem@gmail.com>
2026-03-06 10:46:31 +0100

    Fix vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,21 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(() => {
-  const envDir = '..'
-
-  return {
-    envDir,
-    plugins: [react()],
-    server: {
-      host: true,
-      allowedHosts: true,
-      proxy: {
-        '/api': {
-          target: 'http://localhost:8080',
-          changeOrigin: true,
-        },
+export default defineConfig({
+  envDir: '..',
+  plugins: [react()],
+  server: {
+    host: true,
+    allowedHosts: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
       },
     },
-  }
+  },
 })
